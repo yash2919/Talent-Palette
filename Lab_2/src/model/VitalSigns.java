c774a109f913ef09bf624ccf325eb14d5deda7f9
yash2919 <nahata.y@northeastern.edu>
2023-09-25 18:42:34 -0400

    lab_2

---
+++ Lab_2/src/model/VitalSigns.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author yashn
+ */
+public class VitalSigns {
+    
+private double temperature;
+private double bloodPressure;
+private int pulse;
+private String date;
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public double getBloodPressure() {
+        return bloodPressure;
+    }
+
+    public void setBloodPressure(double bloodPressure) {
+        this.bloodPressure = bloodPressure;
+    }
+
+    public int getPulse() {
+        return pulse;
+    }
+
+    public void setPulse(int pulse) {
+        this.pulse = pulse;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+    @Override
+    public String toString(){
+        return date;
+    }
+
+}
