a291ac7cbb02058524289696b9bc206c4e7bc426
Anshi-Khandelwal <144976536+Anshi-Khandelwal@users.noreply.github.com>
2023-11-26 17:11:30 -0500

    added changes in ui for signup and login

--- webfinalproj/src/Components/Login.js
+++ webfinalproj/src/Components/Login.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function Login() {
   const navigate = useNavigate();
-  const login = async () => {
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
+  const login = async () => {
     try {
       const response = await fetch('http://localhost:3000/user/login', {
         method: 'POST',
@@ -18,7 +20,7 @@ function Login() {
       const data = await response.json();
       if (response.ok) {
         alert(data.message);
-      //  navigate('/home');
+        navigate('/home');
       } else {
         alert(`Login failed: ${data.message}`);
       }
@@ -27,20 +29,63 @@ function Login() {
       alert('An error occurred during login.');
     }
   };
+
   return (
-    <div className="container mt-5">
-      <h2>Login</h2>
-      <form id="loginForm">
-        <div className="mb-3">
-          <label htmlFor="email" className="form-label">Email:</label>
-          <input type="email" className="form-control" id="email" name="email" required />
+    <div className="container-fluid py-5" style={{ 
+        backgroundImage: 'url(/artistic.jpg)', 
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center' // This ensures the image is centered in the container
+      }}>
+      <div className="row min-vh-100 align-items-center justify-content-center">
+        <div className="col-md-6 text-center text-md-start">
+          {/* Marquee tag for scrolling text */}
+          <h1 className="display-4">Talent Palette</h1>
+          <marquee behavior="scroll" direction="left" scrollamount="5">
+            
+            <p className="lead d-none d-md-block">
+            Connect with talents and the world around you on Talent Palette.
+          </p>
+          </marquee>
+          
         </div>
-        <div className="mb-3">
-          <label htmlFor="password" className="form-label">Password:</label>
-          <input type="password" className="form-control" id="password" name="password" required />
+
+        <div className="col-md-4">
+          <div className="card p-4 shadow">
+            <form onSubmit={login}>
+              <div className="mb-3">
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  placeholder="Email or phone number"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="mb-3">
+                <input
+                  type="password"
+                  className="form-control"
+                  id="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="d-grid gap-2 mb-3">
+                <button type="submit" className="btn btn-primary">Log In</button>
+                <button type="button" className="btn btn-link">Forgot password?</button>
+              </div>
+              <div className="text-center">
+                <button type="button" className="btn btn-success">Create new account</button>
+              </div>
+            </form>
+          </div>
         </div>
-        <button type="button" className="btn btn-primary" onClick={login}>Login</button>
-      </form>
+      </div>
     </div>
   );
 }
