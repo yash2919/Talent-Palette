6d464391e53ded6b15a672da0bc0be3ed364082e
yash2919 <nahata.y@northeastern.edu>
2023-11-26 18:21:03 -0500

    fix

--- webfinalproj/src/App.js
+++ webfinalproj/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import SignUpForm from './Components/Signup'; // Ensure this path is correct
-import Login from './Components/Login'
+import SignUpForm from './components/Signup'; // Ensure this path is correct
+import Login from './components/Login'
 import './App.css';
 import logo from './logo.svg';
 import ProfilePage from './components/Profile/ProfilePage';
