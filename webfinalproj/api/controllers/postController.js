7b65adf31cdff9be0ee550093fa77a48c06d17ea
yash2919 <nahata.y@northeastern.edu>
2023-12-03 18:58:46 -0500

    pushing

--- webfinalproj/api/controllers/postController.js
+++ webfinalproj/api/controllers/postController.js
@@ -3,8 +3,8 @@ const postService = require('../services/postService');
 
 async function create(req, res) {
   try {
-    const { email, postName } = req.body;
-    const post = await postService.createPost(email, postName);
+    const { email, postName , postimgUrl } = req.body;
+    const post = await postService.createPost(email, postName,postimgUrl);
 
     res.status(200).json({ message: 'Post creation successful', post });
   } catch (error) {
@@ -14,8 +14,8 @@ async function create(req, res) {
 
 async function getallposts(req, res) {
     try {
-      const { email, postName } = req.body;
-      const post = await postService.getallposts(email, postName);
+      const { email, postName ,postimgUrl} = req.body;
+      const post = await postService.getallposts(email, postName,postimgUrl);
   
       res.status(200).json({ message: 'All Posts', post });
     } catch (error) {
--- webfinalproj/api/services/postService.js
+++ webfinalproj/api/services/postService.js
@@ -1,7 +1,7 @@
 const Post = require('../models/postModel');
 const User =require('../models/userModel')
 const bcrypt = require('bcrypt');
-async function createPost(email, postName) {
+async function createPost(email, postName,postimgUrl) {
    // const user = await User.findOne({ email });
     if(!postName){
         throw new Error('PostName is empty');
@@ -9,14 +9,15 @@ async function createPost(email, postName) {
     else{
         const post = new Post({
             postName,
-            email
+            email,
+            postimgUrl
           });
           await post.save();
           return post;
     }
 }
-async function getallposts(email, postName) {
-    const posts = await Post.find({}, "postName email");
+async function getallposts(email, postName,postimgUrl) {
+    const posts = await Post.find({}, "postName email postimgUrl");
     return posts;
  }
 
