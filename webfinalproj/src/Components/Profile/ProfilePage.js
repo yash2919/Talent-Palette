73ee8ae9afea0c5b75bd941ece4744b757e65071
AakashSanganiNeu <sangani.aak@northeastern.edu>
2023-12-02 20:49:59 -0500

    profile mminimum changes

--- webfinalproj/src/Components/Profile/ProfilePage.js
+++ webfinalproj/src/Components/Profile/ProfilePage.js
@@ -1,19 +1,40 @@
 import React, { useState } from 'react';
 import FileUpload from './FileUpload'; // Adjust the path based on your project structure
-
+import './ProfilePage.css';
 const ProfilePage = () => {
   const [profilePicture, setProfilePicture] = useState(null);
+  const [about, setAbout] = useState('');
+  const [posts, setPosts] = useState([]); // Assuming posts is an array of objects
 
   const handleFileChange = (file) => {
-    // Handle the file as needed, e.g., upload to server or update state
     setProfilePicture(URL.createObjectURL(file));
   };
 
+  const handleAboutChange = (event) => {
+    setAbout(event.target.value);
+  };
+
+  // Placeholder function to add posts
+  const addPost = (postContent) => {
+    const newPost = { content: postContent, id: posts.length + 1 };
+    setPosts([...posts, newPost]);
+  };
+
   return (
-    <div>
-      <h1>Profile Page</h1>
-      <img src={profilePicture} alt="Profile" style={{ width: '150px', height: '150px', borderRadius: '50%' }} />
-      <FileUpload onFileChange={handleFileChange} />
+    <div className="profile-page">
+      <div className="profile-header">
+        <h1>Profile Page</h1>
+        <img src={profilePicture} alt="Profile" className="profile-picture"/>
+        <FileUpload onFileChange={handleFileChange} />
+        <textarea value={about} onChange={handleAboutChange} placeholder="About Me..." className="about-section"></textarea>
+      </div>
+      <div className="posts-section">
+        {posts.map((post) => (
+          <div key={post.id} className="post">
+            {post.content}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
