b83b41470e2947dfde586221d6e8adcb8c93d8ba
AakashSanganiNeu <144560387+AakashSanganiNeu@users.noreply.github.com>
2023-12-04 01:19:41 -0500

    Profile basic details fetching (#40)
    * profile page fix
    
    * profile apis

--- webfinalproj/src/Components/Profile/ProfilePage.js
+++ webfinalproj/src/Components/Profile/ProfilePage.js
@@ -5,10 +5,20 @@ import Navbar from '../Header/Navbar';
 import Card from '../Common/PostCard/FeedCard';
 import image3 from '../../assets/images/artist.jpg';
 import CreatePost from '../../Components/Post';
+import './PortfolioPage.css';
+import UploadWidget from "../Common/UploadWidget/UploadWidget"
 
 const PortfolioPage = () => {
   const navigate = useNavigate();
   const [editMode, setEditMode] = useState(false);
+  const [email, setEmail] = useState("");
+  const [fullName, setFullName] = useState("dfda");
+  const [skills, setSkills] = useState("dfad");
+  const [profileImage, setProfileImage] = useState("");
+  const [gigsInfo, setGigsInfo] = useState("adfad");
+
+  const [postimgUrl, setpostimgUrl] = useState("dfdf");
+
   const [posts, setPosts] = useState([]);
   const samplePost = {
     user: {
@@ -28,54 +38,152 @@ const PortfolioPage = () => {
   });
 
   useEffect(() => {
-    // Flag to check if the component is still mounted
-    let isMounted = true;
 
-    const fetchProfile = async () => {
+    async function fetchUserEmail() {
       try {
-        const response = await axios.get('/user/profile');
+          const response = await fetch('http://localhost:3000', {
+            method: 'GET',
+            credentials: 'include', // Send cookies with the request
+          });
+          
+          if (response.ok) {
+            const data = await response.json();
+            console.log(data.email); // Handle email data as needed
 
-        if (isMounted) {
-          setArtistProfile(response.data);
-        }
-      } catch (error) {
-        console.error('Error fetching profile:', error);
+          if(data.valid===true){
+            // setEmail(data.email);
+            console.log(data.email);
 
-        // Check if the component is still mounted before navigating
-        if (isMounted) {
-          // Instead of navigating to '/', you might want to handle the error differently
-          // For now, let's log the error and leave the user on the current page
-          console.error('Navigation aborted. Component might be unmounted.');
+            setEmail(data.email);
+
+            console.log(email);
+            // fetchProfile();
+          }
+          else{
+            navigate("/home");
+          }
+          
+
+          } else {
+            throw new Error('Failed to fetch email');
+          }
+        } catch (error) {
+          console.error('Error fetching email:', error);
+          // Handle errors
         }
+  }
+
+  // Flag to check if the component is still mounted
+  async function fetchProfile() {
+    try {
+      console.log(email);
+      const response = await fetch(`http://localhost:3000/user/profile/${email}`, {
+        method: "GET",
+        credentials: "include", 
+      });
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        // if(data!=null)
+        setArtistProfile(data);          
+      } else {
+        throw new Error("Failed to fetch profile data");
       }
-    };
+    } catch (error) {
+      console.error("Error fetching profile data:", error);
+      // Handle errors
+    }
+  }
+  
 
-    fetchProfile();
+    fetchUserEmail();
 
     // Cleanup function to set the isMounted flag to false when the component is unmounted
-    return () => {
-      isMounted = false;
-    };
-  }, []);
+    // return () => {
+    //   isMounted = false;
+    // };
+      if (email) {
+        fetchProfile();
+      }
+
+  }, [email]);
 
   const handleEdit = () => {
     setEditMode(true);
   };
 
-  const handleSave = async () => {
-    try {
-      await axios.put('/user/profile', artistProfile);
-      alert('Profile updated successfully!');
-      setEditMode(false);
-      window.location.reload();
-    } catch (error) {
-      console.error('Error updating profile:', error);
-    }
-  };
+  // const handleSave = async () => {
+  //   try {
+  //     await axios.put('/user/profile', artistProfile);
+  //     alert('Profile updated successfully!');
+  //     setEditMode(false);
+  //     window.location.reload();
+  //   } catch (error) {
+  //     console.error('Error updating profile:', error);
+  //   }
+  // };
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setArtistProfile({ ...artistProfile, [name]: value });
+  // const handleChange = (e) => {
+  //   const { name, value } = e.target;
+  //   setArtistProfile({ ...artistProfile, [name]: value });
+  // };
+
+// ...
+
+const handleChange = (e) => {
+  const { name, value } = e.target;
+
+  // Update the corresponding state based on the name attribute
+  switch (name) {
+    case 'fullName':
+      setFullName(value);
+      break;
+    case 'skills':
+      setSkills(value);
+      break;
+    case 'profileImage':
+      setProfileImage(value);
+      break;
+    case 'gigsInfo':
+      setGigsInfo(value);
+      break;
+    // Add other cases as needed
+    default:
+      break;
+  }
+}
+
+const handleSave = async (result)  => {
+  try {
+    const response = await fetch("http://localhost:3000/user/profile", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, fullName, skills, profileImage, gigsInfo }),
+    });
+
+    const data = await response.json();
+    if (response.ok) {
+      alert(data.message);
+      //  navigate('/home');
+    } else {
+      alert(`Post Upload failed: ${data.message}`);
+    }
+  } catch (error) {
+    console.error("Error during Post Upload:", error);
+    alert("An error occurred during Post Upload.");
+  }
+};
+
+// ...
+
+
+  const handleOntTest = (result) => {
+    if(result!=null){
+     // console.log("url"+result);
+      setpostimgUrl(result);
+    }
   };
 
   return (
@@ -83,11 +191,12 @@ const PortfolioPage = () => {
       <Navbar />
       {artistProfile && (
         <div className="profile-container">
+  
           {editMode ? (
             <input
               type="text"
               name="fullName"
-              value={artistProfile.fullName}
+              value={fullName}
               onChange={handleChange}
             />
           ) : (
@@ -97,21 +206,21 @@ const PortfolioPage = () => {
             <input
               type="text"
               name="profileImage"
-              value={artistProfile.profileImage}
+              value={profileImage}
               onChange={handleChange}
             />
           ) : (
             <img
-              src={artistProfile.profileImage || image3}
+              src={profileImage || image3}
               alt={`${artistProfile.fullName}'s Profile`}
               className="profile-image"
             />
           )}
           <div className="about-section">
-            <h2>About</h2>
             {editMode ? (
-              <textarea
+              <input
                 name="about"
+                type="text"
                 value={artistProfile.about}
                 onChange={handleChange}
               />
@@ -120,12 +229,11 @@ const PortfolioPage = () => {
             )}
           </div>
           <div className="skills-section">
-            <h2>Skills</h2>
             {editMode ? (
               <input
                 type="text"
                 name="skills"
-                value={artistProfile.skills}
+                value={artistProfile.fullName}
                 onChange={handleChange}
               />
             ) : (
@@ -133,12 +241,11 @@ const PortfolioPage = () => {
             )}
           </div>
           <div className="gigs-section">
-            <h2>Upcoming Gigs</h2>
             {editMode ? (
               <input
                 type="text"
                 name="gigsInfo"
-                value={artistProfile.gigsInfo}
+                value={gigsInfo}
                 onChange={handleChange}
               />
             ) : (
@@ -152,18 +259,16 @@ const PortfolioPage = () => {
             <button onClick={handleEdit}>Edit</button>
           )}
           <div className="posts-section">
-            <h2>Posts Shared</h2>
-            {/* ... post mapping */}
+            {/* ... (other components) */}
           </div>
         </div>
       )}
       {
         <div>
-          <CreatePost userProfilePicture={image3} />
-          <Card {...samplePost} />
-        </div>}
+          {/* ... (other components) */}
+        </div>
+      }
     </div>
   );
-};
-
+}  
 export default PortfolioPage;
