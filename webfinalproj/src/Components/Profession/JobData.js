2b34e06876036e09f0a0287ed7db650f90d97897
yash2919 <nahata.y@northeastern.edu>
2023-12-05 21:21:12 -0500

    added backend

--- webfinalproj/src/Components/Profession/JobData.js
+++ webfinalproj/src/Components/Profession/JobData.js
@@ -1,31 +1,70 @@
-// jobData.js
-const jobData = {
-    Artist: [
-      { id: 'a1', title: 'Graphic Designer', description: 'Description for Graphic Designer job...' },
-      { id: 'a2', title: 'Painter', description: 'Description for Painter job...' },
-      { id: 'a3', title: 'Illustrator', description: 'Description for Illustrator job...' },
-      // ... other job listings
-    ],
+// // jobData.js
+// const jobData = {
+//     Artist: [
+//       { id: 'a1', title: 'Graphic Designer', description: 'Description for Graphic Designer job...' },
+//       { id: 'a2', title: 'Painter', description: 'Description for Painter job...' },
+//       { id: 'a3', title: 'Illustrator', description: 'Description for Illustrator job...' },
+//     ],
 
 
 
-    Dancer: [
-       { id: 'a1', title: 'Hip Hop', description: 'Description' },
-       { id: 'a2', title: 'Painter', description: 'Description for Painter job...' },
-       { id: 'a3', title: 'Illustrator', description: 'Description for Illustrator job...' },
-      ],
-      Musician: [
-        { id: 'a1', title: 'Soothing Songs', description: 'Description' },
-       { id: 'a2', title: 'Jazz', description: 'Description for Painter job...' },
-       { id: 'a3', title: 'Illustrator', description: 'Description for Illustrator job...' },
+//     Dancer: [
+//        { id: 'a1', title: 'Hip Hop', description: 'Description' },
+//        { id: 'a2', title: 'Painter', description: 'Description for Painter job...' },
+//        { id: 'a3', title: 'Illustrator', description: 'Description for Illustrator job...' },
+//       ],
+//       Musician: [
+//         { id: 'a1', title: 'Soothing Songs', description: 'Description' },
+//        { id: 'a2', title: 'Jazz', description: 'Description for Painter job...' },
+//        { id: 'a3', title: 'Illustrator', description: 'Description for Illustrator job...' },
        
-      ],
-      'My Applications': [
-        // ... your applications
-      ],
-    };
-    // ... other categories
+//       ],
+//       'My Applications': [
+//         { id: 'a1', title: 'Soothing Songs', description: 'Description' },
+//         { id: 'a2', title: 'Jazz', description: 'Description for Painter job...' },
+//         { id: 'a3', title: 'Illustrator', description: 'Description for Illustrator job...' },
+    
+//       ],
+//     };
+
 
   
-  export default jobData;
+//   export default jobData;
+const jobData = async () => {
+
+  try {
+    const response = await fetch('http://localhost:3000/jobs/getalljobs'); // Replace '/api/jobs' with your API endpoint
+    if (!response.ok) {
+      throw new Error('Network response was not ok.');
+    }
+  
+    const data = await response.json();
+   // console.log(data);
+
+    // Organize data by jobType
+    const organizedData = {};
+
+    data.jobs.map(job => {
+      if (!organizedData[job.jobType]) {
+        organizedData[job.jobType] = [];
+      }
+      if(job.jobstatus==="open"){
+      organizedData[job.jobType].push({
+        id: job._id,
+        title: job.jobName,
+        description: job.jobDesc,
+        image:job.jobimgUrl
+        // Add other fields as needed
+      });}
+    });
+
+    return organizedData;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return {};
+  }
+};
+
+export default jobData;
+
   
--- webfinalproj/src/Components/Profession/ProfessionPage.js
+++ webfinalproj/src/Components/Profession/ProfessionPage.js
@@ -1,13 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import JobListings from './JobListings';
 import JobDetails from './JobDetails';
 import CategoryButtons from './CategoryButtons'; 
 import jobData from './JobData';
 import './Profession.css'
-function ProfessionPage() {
+const ProfessionPage=()=> {
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [selectedJob, setSelectedJob] = useState(null);
+  const [alljobs, setallJobs] = useState([{}]);
 
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const fetchedData = await jobData(); // Call the jobData function to fetch data
+       console.log(fetchedData);
+        setallJobs(fetchedData);
+      } catch (error) {
+        console.error('Error fetching jobs:', error);
+      }
+    };
+
+    fetchJobs();
+  }, []); 
   const handleCategorySelect = (category) => {
     setSelectedCategory(category);
     setSelectedJob(null); 
@@ -18,7 +32,7 @@ function ProfessionPage() {
       <CategoryButtons onSelectCategory={handleCategorySelect} />
       {!selectedJob && selectedCategory ? (
         <JobListings 
-          jobs={jobData[selectedCategory]} 
+          jobs={alljobs[selectedCategory]} 
           onSelectJob={setSelectedJob} 
         />
       ) : selectedJob ? (
