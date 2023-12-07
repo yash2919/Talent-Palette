e5d372734e1c13698efcdf4a389194f618b1baca
AakashSanganiNeu <sangani.aak@northeastern.edu>
2023-12-07 06:14:22 -0500

    new

---
+++ start.sh
@@ -0,0 +1,22 @@
+
+
+npm install
+
+npm install bcrypt mongoose
+
+npm install react-type-animation
+
+npm install @fortawesome/fontawesome-svg-core @fortawesome/react-fontawesome @fortawesome/free-regular-svg-icons
+
+node server.js
+
+
+
+
+# git fetch origin main
+
+# git rebase origin/main
+
+# after resolving conflicts
+
+# git rebase --continue 
